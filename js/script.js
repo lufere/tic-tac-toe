3df9c277a265316f11249e080bb6536a996f2aae
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-04-16 06:35:13 -0700

    Developed gameflow logic which allows to click empty spaces and places either an X or a O alternatively

--- js/script.js
+++ js/script.js
@@ -8,13 +8,22 @@ const Gameboard = (()=>{
             grid[i].innerHTML = boardstatus[i];
         }
     }
+    return {render}
+})()
+
+Gameflow = (()=>{
     document.querySelectorAll('.tile').forEach(tile => tile.addEventListener('click', getPosition));
+    var turn = true;
+    function toggleTurn(){
+        turn = !turn;
+    }
     function getPosition(event) { 
-        position = (event.target).dataset.spot;
-        alert (position);
-        return position;
+        position = event.target.dataset.spot;
+        if(event.target.innerHTML == ""){
+            turn? player1.place(position): player2.place(position);
+            toggleTurn();
+        } 
       }
-    return {render,getPosition}
 })()
 
 Gameboard.render();
@@ -27,4 +36,5 @@ const Player = (name, mark)=>{
 }
 
 const player1 = Player("l","X");
+const player2 = Player("r","O");
 player1.place(4);
